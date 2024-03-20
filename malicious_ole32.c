7d8c3cd2c16cc1fd0914a2c23c5d824ddc4affb8
BooRuleDie <booruledie@gmail.com>
2024-03-20 13:04:22 +0300

    some changes

--- malicious_ole32.c
+++ malicious_ole32.c
@@ -1,10 +1,9 @@
 #include <windows.h>
-#include <stdio.h>
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
     switch (fdwReason) {
         case DLL_PROCESS_ATTACH:
-            OutputDebugStringA("I'm not the original ole32.dll >:)");
+            MessageBoxA(NULL, "I'm not the original ole32.dll >:)", "Custom DLL Message", MB_OK);
             break;
         case DLL_PROCESS_DETACH:
             break;
